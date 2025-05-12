46d7da7056218a172eef4ff73b8388e6c9492a02
Kaif2596 <kaifansari1808@gmail.com>
2025-05-12 23:28:02 +0530

    Update main.py

--- main.py
+++ main.py
@@ -80,3 +80,215 @@ def visualize_prediction(model, img_path, threshold=0.5):
 # Replace with your actual image path or upload via Colab
 img_path = "D:\Projects\Bi Polar Assignment\image05.jpg"  # example image name
 visualize_prediction(model, img_path, threshold=0.5)
+
+
+################################################################################
+##This is Google Colab code that I add here##
+
+import torch
+import torch.nn as nn
+from torchvision import models
+
+# Load pre-trained MobileNetV2
+mobilenet = models.mobilenet_v2(pretrained=True)
+
+# Remove the classifier head
+backbone = mobilenet.features
+backbone.out_channels = 1280  # MobileNetV2 output feature size
+
+class SSDHead(nn.Module):
+    def __init__(self, in_channels, num_classes):
+        super(SSDHead, self).__init__()
+        self.class_head = nn.Conv2d(in_channels, num_classes * 4, kernel_size=3, padding=1)
+        self.box_head = nn.Conv2d(in_channels, 4 * 4, kernel_size=3, padding=1)
+
+    def forward(self, x):
+        class_preds = self.class_head(x)  # (N, num_classes*4, H, W)
+        box_preds = self.box_head(x)      # (N, 4*4, H, W)
+        return class_preds, box_preds
+
+# Define number of object classes (20 Pascal VOC classes + 1 background)
+num_classes = 21
+ssd_head = SSDHead(in_channels=1280, num_classes=num_classes)
+
+class SSDModel(nn.Module):
+    def __init__(self, backbone, ssd_head):
+        super(SSDModel, self).__init__()
+        self.backbone = backbone
+        self.ssd_head = ssd_head
+
+    def forward(self, x):
+        features = self.backbone(x)
+        class_preds, box_preds = self.ssd_head(features)
+        return class_preds, box_preds
+
+# Initialize the model
+model = SSDModel(backbone, ssd_head)
+
+# Move model to GPU if available
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+model = model.to(device)
+
+from torchvision.datasets import VOCDetection
+from torchvision import transforms
+from torch.utils.data import Dataset, DataLoader
+import os
+import xml.etree.ElementTree as ET
+import numpy as np
+import cv2
+
+# Define object class names
+VOC_CLASSES = ['aeroplane', 'bicycle', 'bird', 'boat', 'bottle',
+               'bus', 'car', 'cat', 'chair', 'cow',
+               'diningtable', 'dog', 'horse', 'motorbike', 'person',
+               'pottedplant', 'sheep', 'sofa', 'train', 'tvmonitor']
+
+class VOCDataset(Dataset):
+    def __init__(self, root, image_set='train', year='2007', transform=None):
+        self.dataset = VOCDetection(root=root, year=year, image_set=image_set, download=True)
+        self.transform = transform
+
+    def __len__(self):
+        return len(self.dataset)
+
+    def __getitem__(self, idx):
+        img, annotation = self.dataset[idx]
+        boxes = []
+        labels = []
+
+        for obj in annotation['annotation']['object']:
+            bbox = obj['bndbox']
+            label = VOC_CLASSES.index(obj['name'])
+            box = [int(bbox['xmin']), int(bbox['ymin']), int(bbox['xmax']), int(bbox['ymax'])]
+            boxes.append(box)
+            labels.append(label)
+
+        img = np.array(img)
+
+        if self.transform:
+            img = self.transform(img)
+
+        boxes = torch.tensor(boxes, dtype=torch.float32)
+        labels = torch.tensor(labels, dtype=torch.int64)
+
+        return img, boxes, labels
+
+# Define image transforms (resize to 300x300 for SSD and normalize)
+transform = transforms.Compose([
+    transforms.ToPILImage(),
+    transforms.Resize((300, 300)),
+    transforms.ToTensor(),
+    transforms.Normalize([0.5, 0.5, 0.5], [0.5, 0.5, 0.5])
+])
+
+# Load dataset
+train_dataset = VOCDataset(root='.', image_set='train', year='2007', transform=transform)
+train_loader = DataLoader(train_dataset, batch_size=4, shuffle=True, collate_fn=lambda x: tuple(zip(*x)))
+
+from torchvision.datasets import VOCDetection
+from torchvision import transforms
+from torch.utils.data import Dataset, DataLoader
+import os
+import xml.etree.ElementTree as ET
+import numpy as np
+import cv2
+
+# Define object class names
+VOC_CLASSES = ['aeroplane', 'bicycle', 'bird', 'boat', 'bottle',
+               'bus', 'car', 'cat', 'chair', 'cow',
+               'diningtable', 'dog', 'horse', 'motorbike', 'person',
+               'pottedplant', 'sheep', 'sofa', 'train', 'tvmonitor']
+
+class VOCDataset(Dataset):
+    def __init__(self, root, image_set='train', year='2007', transform=None):
+        self.dataset = VOCDetection(root=root, year=year, image_set=image_set, download=True)
+        self.transform = transform
+
+    def __len__(self):
+        return len(self.dataset)
+
+    def __getitem__(self, idx):
+        img, annotation = self.dataset[idx]
+        boxes = []
+        labels = []
+
+        for obj in annotation['annotation']['object']:
+            bbox = obj['bndbox']
+            label = VOC_CLASSES.index(obj['name'])
+            box = [int(bbox['xmin']), int(bbox['ymin']), int(bbox['xmax']), int(bbox['ymax'])]
+            boxes.append(box)
+            labels.append(label)
+
+        img = np.array(img)
+
+        if self.transform:
+            img = self.transform(img)
+
+        boxes = torch.tensor(boxes, dtype=torch.float32)
+        labels = torch.tensor(labels, dtype=torch.int64)
+
+        return img, boxes, labels
+
+# Define image transforms (resize to 300x300 for SSD and normalize)
+transform = transforms.Compose([
+    transforms.ToPILImage(),
+    transforms.Resize((300, 300)),
+    transforms.ToTensor(),
+    transforms.Normalize([0.5, 0.5, 0.5], [0.5, 0.5, 0.5])
+])
+
+# Load dataset
+train_dataset = VOCDataset(root='.', image_set='train', year='2007', transform=transform)
+train_loader = DataLoader(train_dataset, batch_size=4, shuffle=True, collate_fn=lambda x: tuple(zip(*x)))
+
+import torch.nn as nn
+import torch.optim as optim
+from tqdm import tqdm
+
+
+def train(model, dataloader, criterion_cls, criterion_bbox, optimizer, num_epochs=5):
+    model.train()  # Set model to training mode
+
+    for epoch in range(num_epochs):
+        epoch_loss = 0.0
+        for images, boxes, labels in tqdm(dataloader):
+            images = torch.stack(images).to(device)
+            boxes = list(b.to(device) for b in boxes)
+            labels = list(l.to(device) for l in labels)
+
+            optimizer.zero_grad()  # Zero the gradients before backward pass
+
+            # Forward pass through the model
+            pred_classes, pred_boxes = model(images)
+
+            # Simulate only one target per image for simplicity (you can enhance this later)
+            target_classes = torch.stack([l[0] for l in labels])  # Only using one target class
+            target_boxes = torch.stack([b[0] for b in boxes])  # Only using one target box
+
+            # Compute loss
+            cls_loss = criterion_cls(pred_classes, target_classes)
+            box_loss = criterion_bbox(pred_boxes, target_boxes)
+            loss = cls_loss + box_loss
+
+            # Backward pass
+            loss.backward()
+            optimizer.step()
+
+            epoch_loss += loss.item()
+
+        # Print loss for each epoch
+        print(f"Epoch [{epoch + 1}/{num_epochs}], Loss: {epoch_loss:.4f}")
+
+model = ssd_model.to(device)
+
+# Define loss functions
+classification_loss_fn = nn.CrossEntropyLoss()
+bbox_loss_fn = nn.SmoothL1Loss()
+
+# Optimizer
+optimizer = optim.Adam(model.parameters(), lr=1e-4)
+
+# Train model
+train(model, train_loader, criterion_cls=classification_loss_fn, criterion_bbox=bbox_loss_fn, optimizer=optimizer, num_epochs=10)
+
+
